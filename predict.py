ba6cdc93b0a6295f038264a3f351ae9facd8ba4b
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2024-03-11 16:07:13 +0100

    Add files via upload

--- predict.py
+++ predict.py
@@ -5,27 +5,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import data_loader
 import custom_layers_BAM as cl
+import generator_image
 
 tf.keras.backend.set_floatx('float32')
 ddtype = tf.float32
 
 custom_objects = {
+    'weight_binary_loss':0.8,
     'layer_N_M_d_1_to_N_M_d_C_residual': cl.layer_N_M_d_1_to_N_M_d_C_residual,
     'layer_N_M_d_C_attention_features_for_each_sample': cl.layer_N_M_d_C_attention_features_for_each_sample,
     'layer_N_M_d_C_attention_samples_for_each_feature': cl.layer_N_M_d_C_attention_samples_for_each_feature,
     'layer_N_C_d_d_bilinear_attention_cov2cor_spd':cl.layer_N_C_d_d_bilinear_attention_cov2cor_spd,
     'layer_N_C_d_d_spd_activation_scaled':cl.layer_N_C_d_d_spd_activation_scaled,
-    'layer_N_c_d_d_to_N_d_d_3_LogEig_softmax2':cl.layer_N_c_d_d_to_N_d_d_3_LogEig_softmax2,
+    'layer_N_c_d_d_to_N_d_d_3_softmax':cl.layer_N_c_d_d_to_N_d_d_3_LogEig_softmax2(7),
     'layer_channels_dense_res_N_M_d_c':cl.layer_channels_dense_res_N_M_d_c,
     'data_N_M_d_c_to_cov_N_c_d_d':cl.data_N_M_d_c_to_cov_N_c_d_d,
     'frob': cl.frob,
     'lam_init_eps': cl.lam_init_eps,
     'l1_constraintLessEqual': cl.l1_constraintLessEqual,
-    'layer_N_M_d_1_to_N_x_x_C_conv': cl.layer_N_M_d_1_to_N_x_x_C_conv,
-    'layer_N_c_d_d_to_N_d_d_3_LogEig': cl.layer_N_c_d_d_to_N_d_d_3_LogEig,
-    'layer_dense': cl.layer_dense,
-    'layer_softmax2': cl.layer_softmax2,
-    'baseline': cl.baseline
+    'layer_N_M_d_1_to_N_x_x_C_conv': cl.layer_N_M_d_1_to_N_x_x_C_conv(out_filters=100),
+    'layer_N_c_d_d_to_N_d_d_3_LogEig': cl.layer_N_c_d_d_to_N_d_d_3_LogEig(7),
+    'layer_dense': cl.layer_dense(7),
+    'layer_softmax2': cl.layer_softmax2(7),
+    'layer_baseline': cl.baseline
 }
 
 # load the pretrained model
@@ -80,6 +82,11 @@ classes_pred = []
 
 pixels_array = np.array(pixels, dtype=np.float32)
 pixels_array = pixels_array.reshape((len(pixels_array), standard_size[0], standard_size[1]))
+
+# img_gen = generator_image.DataGenerator_image(img_folder, classes_true, names, batch_size=128, num_classes=len(classes))
+# evaluation = model.evaluate(img_gen)
+
+# """
 predictions = model.predict(pixels_array)
 for prediction in predictions:
     predicted_class = np.argmax(prediction)     # find the most possible class for each image
@@ -121,3 +128,5 @@ plt.title('Confusion Matrix (count)')
 
 plt.savefig('confusion_matrix.png')
 # plt.show()
+
+# """
